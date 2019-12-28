7437445821732cdf24f908851d0cc02f0ceb1c4f
Derek Yee <dereky007@gmail.com>
2019-12-28 13:37:44 -0800

    initial setup menu

--- src/main_menu.java
+++ src/main_menu.java
@@ -1,5 +1,8 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -12,11 +15,19 @@ public class main_menu extends Application
 	}
 
 	Label welcomeL=new Label("Welcome to the inconvenient phone number entry application!"); 
-	VBox mainMenu=new VBox(10);
+	BorderPane root=new BorderPane();
+	VBox menu=new VBox(10);
 	GridPane options=new GridPane(); 
-	public void start(Stage arg0) throws Exception
+	public void start(Stage myStage) 
 	{
+		Scene myScene=new Scene(root,500,500);
+		menu.getChildren().addAll(welcomeL,options);
+		menu.setAlignment(Pos.CENTER);
+		root.setCenter(menu);
 		
+		
+		myStage.setScene(myScene);
+		myStage.show();
 		
 	}
 	
