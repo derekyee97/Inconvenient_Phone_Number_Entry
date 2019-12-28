e11df1448abfb24751b15de3fe0ed51d0b9b521b
Derek Yee <dereky007@gmail.com>
2019-12-28 13:29:37 -0800

    added initial setup

---
+++ src/main_menu.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class main_menu extends Application 
+{
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+
+	Label welcomeL=new Label("Welcome to the inconvenient phone number entry application!"); 
+	VBox mainMenu=new VBox(10);
+	GridPane options=new GridPane(); 
+	public void start(Stage arg0) throws Exception
+	{
+		
+		
+	}
+	
+}
