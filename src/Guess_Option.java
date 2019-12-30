5d80fdede4eae083f41d6c11c2c7a021c2be95dc
Derek Yee <dereky007@gmail.com>
2019-12-29 16:30:21 -0800

    added guess option functionality

---
+++ src/Guess_Option.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class Guess_Option 
+{
+	public static void createGuessPage(VBox page)
+	{
+		Label a=new Label("Is this your number?");
+		Random genDigit=new Random(); 
+		Label resultGuess=new Label(genNumber());
+		page.getChildren().removeAll(); 
+		page.getChildren().addAll(a,resultGuess);
+		
+		
+	}
+	public static String genNumber()
+	{
+		Random genDigit=new Random(); 
+		StringBuilder generatedNum=new StringBuilder();
+		generatedNum.append("("); 
+		for(int i=1;i<=10;i++)
+		{
+			generatedNum.append(Integer.toString(genDigit.nextInt(10))); 
+			if(i==3)
+			{
+				generatedNum.append(") - "); 
+			}
+			else if(i==6)
+			{
+				generatedNum.append(" - ");
+			}
+		}
+		return generatedNum.toString();
+	}
+}
--- src/main_menu.java
+++ src/main_menu.java
@@ -1,9 +1,12 @@
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,13 +18,17 @@ public class main_menu extends Application
 	}
 
 	Label welcomeL=new Label("Welcome to the inconvenient phone number entry application!"); 
+	Label instructionL=new Label("Please select a choice: ");
 	BorderPane root=new BorderPane();
 	VBox menu=new VBox(10);
 	GridPane options=new GridPane(); 
+	Button guessB=new Button("Guess");
 	public void start(Stage myStage) 
 	{
 		Scene myScene=new Scene(root,500,500);
-		menu.getChildren().addAll(welcomeL,options);
+		options.setAlignment(Pos.CENTER);
+		options.add(guessB, 0,0);
+		menu.getChildren().addAll(welcomeL,instructionL,options);
 		menu.setAlignment(Pos.CENTER);
 		root.setCenter(menu);
 		
@@ -29,6 +36,23 @@ public class main_menu extends Application
 		myStage.setScene(myScene);
 		myStage.show();
 		
+		guessB.setOnAction(event->{
+			VBox guessWindow=new VBox(); 
+			guessWindow.setAlignment(Pos.CENTER);
+			Guess_Option.createGuessPage(guessWindow);
+			root.setCenter(guessWindow);
+			HBox buttonHolder=new HBox(10); 
+			Button yesB=new Button("Yes");Button noB=new Button("No"); 
+			buttonHolder.getChildren().addAll(yesB,noB);
+			buttonHolder.setAlignment(Pos.CENTER);
+			guessWindow.getChildren().add(buttonHolder);
+			yesB.setOnAction(event2->{
+				root.setCenter(menu);
+			});
+			noB.setOnAction(event3->{
+				((Label) guessWindow.getChildren().get(1)).setText(Guess_Option.genNumber());
+			});
+		});
 	}
 	
 }
