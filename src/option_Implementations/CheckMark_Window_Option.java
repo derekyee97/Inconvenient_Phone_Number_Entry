b4e399ee8a8f45ebbf992f784dbff0140d88877e
Derek Yee <dereky007@gmail.com>
2020-02-26 11:55:42 -0800

    finished check number option

--- src/option_Implementations/CheckMark_Window_Option.java
+++ src/option_Implementations/CheckMark_Window_Option.java
@@ -3,11 +3,14 @@ package option_Implementations;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -20,14 +23,15 @@ public class CheckMark_Window_Option
 	{
 
 		@Override
+		//this event handler will check if the user drawn a number after every check click
 		public void handle(ActionEvent event) 
 		{
-			CheckBox a=(CheckBox) event.getSource(); 
+			CheckBox a=(CheckBox) event.getSource();     //get box that was clicked and link to gridpane parent
 			GridPane parent=(GridPane) a.getParent(); 
-			int num=getDrawnNumber(parent); 
+			int num=getDrawnNumber(parent);            
 			if(num<0)
 			{
-				phoneNumber.setCharAt(parent.getId().charAt(0)-'0','X');
+				phoneNumber.setCharAt(parent.getId().charAt(0)-'0','X');  //parent id links to spot in phonenumber (global var) 
 			}
 			else
 			{
@@ -41,6 +45,7 @@ public class CheckMark_Window_Option
 	};
 	public static void createCheckMarkWindow(VBox window)
 	{
+		//defining gridpanes to hold each digit of phone number, and filling with checkboxes 
 		GridPane firstD=new GridPane();genBoxes(firstD);GridPane secondD=new GridPane();genBoxes(secondD);
 		GridPane thirdD=new GridPane();genBoxes(thirdD);GridPane fourthD=new GridPane();genBoxes(fourthD);
 		GridPane fifthD=new GridPane();genBoxes(fifthD);GridPane sixthD=new GridPane();genBoxes(sixthD);
@@ -49,9 +54,12 @@ public class CheckMark_Window_Option
 		firstD.setId("0");secondD.setId("1");thirdD.setId("2");fourthD.setId("3");fifthD.setId("4");
 		sixthD.setId("5");seventhD.setId("6");eighthD.setId("7");ninthD.setId("8");tenthD.setId("9");
 		HBox holder=new HBox(10,firstD,secondD,thirdD,fourthD,fifthD,sixthD,seventhD,eighthD,ninthD,tenthD); 
-		holder.setAlignment(Pos.CENTER);
+		holder.setAlignment(Pos.CENTER); holder.setPadding(new Insets(10));
 		setText(phoneL, phoneNumber);
-		window.getChildren().addAll(phoneL,holder); 
+		Image keyI=new Image("/resources/key.PNG");
+		ImageView key=new ImageView(keyI);
+		Label instructL=new Label("Please draw each digit with check marks as displayed above");
+		window.getChildren().addAll(key, instructL,phoneL,holder); 
 		firstD.setOnMouseClicked(event->{
 			System.out.println("Clicked");
 		});
@@ -77,7 +85,7 @@ public class CheckMark_Window_Option
 		}
 		
 	}
-	private static int getDrawnNumber(GridPane digit)
+	private static  int getDrawnNumber(GridPane digit)
 	{
 		boolean[] check=new boolean[28]; 
 		int count=-1;
@@ -140,17 +148,72 @@ public class CheckMark_Window_Option
 		//check 4
 		else if(check[3]==true && check[7]==true && check[11]==true && check[15]==true 
 				&& check[12]==true && check[13]==true && check[14]==true && check[9]==true && 
-				check[5]==true && check[17]==true && check[21]==true && check[25]==true)
+				check[5]==true && check[17]==true && check[21]==true && check[25]==true &&
+				check[0]!=true && check[4]!=true && check[8]!=true && check[1]!=true && check[2]!=true &&
+				check[6]!=true && check[10]!=true && check[18]!=true && check[22]!=true && check[26]!=true &&
+				check[19]!=true && check[16]!=true && check[23]!=true && check[20]!=true && check[27]!=true &&
+				check[24]!=true)
 		{
 			return 4;
 		}
 		//check 5
-		else if(check[3]==true && check[7]==true && check[11]==true && check[15]==true 
-				&& check[12]==true && check[13]==true && check[14]==true && check[9]==true && 
-				check[5]==true && check[17]==true && check[21]==true && check[25]==true)
+		else if(check[3]==true && check[0]==true && check[1]==true && check[2]==true 
+				&& check[7]==true && check[11]==true && check[15]==true && check[12]==true && 
+				check[13]==true && check[18]==true && check[22]==true && check[25]==true && check[24]==true 
+				&& check[23]==true &&
+				check[4]!=true && check[5]!=true && check[6]!=true && check[8]!=true && check[9]!=true && 
+				check[10]!=true && check[14]!=true && check[19]!=true && check[16]!=true && check[17]!=true &&
+				check[20]!=true && check[21]!=true && check[27]!=true && check[26]!=true)
+		{
+			return 5;
+		}
+		//check 6
+		else if(check[0]==true && check[1]==true && check[6]==true && check[7]==true 
+				&& check[15]==true && check[11]==true && check[12]==true && check[13]==true && 
+				check[18]==true && check[22]==true && check[24]==true && check[25]==true && check[23]==true 
+				&& check[19]==true && 
+				check[3]!=true && check[2]!=true && check[4]!=true && check[5]!=true && check[8]!=true &&
+				check[9]!=true && check[10]!=true && check[14]!=true && check[16]!=true && check[17]!=true &&
+				check[20]!=true && check[21]!=true && check[27]!=true && check[26]!=true)
+		{
+			return 6;
+		}
+		else if(check[3]==true && check[7]==true && check[1]==true && check[2]==true 
+				&& check[0]==true && check[6]==true && check[10]==true && check[13]==true && 
+				check[17]==true && check[20]==true && check[24]==true &&
+				check[4]!=true && check[5]!=true && check[8]!=true && check[9]!=true && 
+				check[11]!=true && check[15]!=true && check[12]!=true && check[19]!=true &&
+				check[16]!=true && check[23]!=true && check[27]!=true && check[21]!=true &&
+				check[22]!=true && check[25]!=true && check[26]!=true && check[18]!=true &&
+				check[14]!=true)
+		{
+			return 7;
+		}
+		else if(check[0]==true && check[1]==true && check[7]==true && check[11]==true 
+				&& check[6]==true && check[10]==true && check[12]==true && check[13]==true && 
+				check[18]==true && check[22]==true && check[19]==true && check[23]==true && 
+				check[24]==true && check[25]==true &&
+				check[3]!=true && check[2]!=true && check[4]!=true && check[5]!=true && check[8]!=true &&
+				check[9]!=true && check[15]!=true && check[14]!=true && check[16]!=true && check[17]!=true &&
+				check[20]!=true && check[21]!=true && check[27]!=true && check[26]!=true)
+				
+		{
+			return 8; 
+		}
+		else if(check[0]==true && check[1]==true && check[7]==true && check[11]==true 
+				&& check[6]==true && check[10]==true && check[12]==true && check[13]==true && 
+				check[18]==true && check[14]==true && check[22]==true && check[23]==true && 
+				check[24]==true && check[25]==true && 
+				check[3]!=true && check[2]!=true && check[4]!=true && check[5]!=true && check[8]!=true &&
+				check[9]!=true && check[15]!=true && check[19]!=true && check[16]!=true && check[17]!=true &&
+				check[20]!=true && check[21]!=true && check[27]!=true && check[26]!=true)
+				
+		{
+			return 9;
+		}
 		return -1;
 	}
-	private static void setText(Label label, StringBuilder num) //
+	private static void setText(Label label, StringBuilder num) 
 	{
 		StringBuilder result=new StringBuilder("("); 
 		result.append(num.charAt(0));result.append(num.charAt(1));result.append(num.charAt(2)); 
